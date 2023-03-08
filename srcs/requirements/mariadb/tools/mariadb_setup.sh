a4f53471b5c96faf1bac6e036504074a7c7cdb5c
pearpearB <smilyoung@gmail.com>
2023-03-08 13:44:05 +0900

    [Style] 변수 중괄호로 묶어주기

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -4,13 +4,13 @@ if [ ! -d "/var/lib/mysql/wordpress" ]; then
 
 	service mysql start
 
-	mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME; \
-		CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PW'; \
-		GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%'; \
-		ALTER USER '$DB_ROOT'@'localhost' IDENTIFIED BY '$DB_ROOT_PW'; \
+	mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}; \
+		CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PW}'; \
+		GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%'; \
+		ALTER USER '${DB_ROOT}'@'localhost' IDENTIFIED BY '${DB_ROOT_PW}'; \
 		FLUSH PRIVILEGES;"
 		
-	mysqladmin -u$DB_ROOT -p$DB_ROOT_PW shutdown
+	mysqladmin -u${DB_ROOT} -p${DB_ROOT_PW} shutdown
 
 fi
 
